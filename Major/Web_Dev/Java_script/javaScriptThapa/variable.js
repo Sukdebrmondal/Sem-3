c1cd07a82a6ea07bdf4eaab459d60c0af105cf65
Suku <sukdebdhalla@gmail.com>
2024-07-27 20:41:30 +0530

    new files

---
+++ Major/Web_Dev/Java_script/javaScriptThapa/variable.js
@@ -0,0 +1,22 @@
+// valid
+// var my_firstName="Sukdeb";
+// console.log(my_firstName);
+// var _MyLastName="Mondal";
+// console.log(_MyLastName);
+/* not valid
+Var 123myAge=25;
+console.log(123myAge);*/
+// var $cityName="india"
+// console.log($cityName);
+/* not valid
+var my@Email="Sukdeb02022@gmail.com";
+console.log(my@Email);*/
+//? Types of Primitive Data types
+//  Number: ex-
+// var mynum=5;
+// console.log(mynum);
+//? String ex
+// var mystr= "sukdeb";
+// console.log(mystr);
+var myname="sukdeb";
+console.log(typeof myname);
