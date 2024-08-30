dea7abbc0b0f3a5b96e55b5a6ebdeef834c4ead4
Suku <sukdebdhalla@gmail.com>
2024-08-31 01:57:07 +0530

    prodj

--- Major/Web_Dev/Django/mypro/newapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class member(models.Model):
-    firstname=models.CharField(max_length=100)
-    lasttname=models.CharField(max_length=100)
-    country=models.CharField(max_length=100)
